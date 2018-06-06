db76410a65321037539c3f07717fbb5ab501ac56
Gian Carlo Vegas R <gvegas@belatrixsf.com>
2018-06-06 16:15:16 -0500

    added response text

--- controllers/lenddo/lenddo.js
+++ controllers/lenddo/lenddo.js
@@ -26,7 +26,7 @@ router
 
         strategy.queue(data)
             .then((success) => {
-                res.status(success ? 200 : 400).send();
+                res.status(success ? 200 : 400).send(success);
             })
             .catch(() => {
                 res.status(400).send();
